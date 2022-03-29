4d001f52e116004cc8cba834b7061e78528b9044
Salizn <97919315+Salizn@users.noreply.github.com>
2022-03-28 19:22:34 -0600

    javascript almost done
    just need to incorporate user input for how many stars to show

--- random.js
+++ random.js
@@ -1,1 +1,6 @@
-// JavaScript source code
+function changeCSS() {
+	document.getElementById('cssButton').style.backgroundColor = "#FFCB9A";
+}
+function makeStars() {
+	document.getElementById('star-container-one').style.opacity = 1;
+}
